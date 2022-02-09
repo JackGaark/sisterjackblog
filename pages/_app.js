8b61bb2a6ef3500bc6db76643ec95d8e3e768c4e
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2022-02-09 10:58:18 -0500

    corrected typo in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import { StaticKitProvider } from "@statickit/react";
 import { DefaultSeo } from "next-seo";
 import "../styles/global.css";
 import SEO from "../lib/next-seo.config";
-import Fonts from '../components/fonts';
+import Fonts from '../components/Fonts';
 import theme from '../styles/theme';
 import Head from 'next/head'
 
